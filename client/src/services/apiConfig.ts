04e4f2129a889073d74036cf6710441720d54b04
henashke <49614066+henashke@users.noreply.github.com>
2026-03-07 16:03:58 +0200

    Merge pull request #9
    * Debugging localhost api calls. Added some stuff to make sure render d…
    
    * Fixed the double // bug. Adding vazana for good luck
    
    * Removed vazana. He indeed gave good luck

--- client/src/services/apiConfig.ts
+++ client/src/services/apiConfig.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 export const getApi = (path: string): AxiosInstance => {
-  console.log(import.meta.env.PROD);
-  const apiPath = `${window.location.origin}/api/${path}`
+  const apiPath = `${window.location.origin}/api${path}`
   return axios.create({
     baseURL: `${apiPath}`,
     headers: {
